cc4a95e30dccfceee88dc22c0f552bb5b97b25de
1708cjy <2287497792@qq.com>
2020-10-08 10:34:39 +0800

    目前成功实现产生4个不同的数并能从小到大排序,实现一次排序比较,已完成

--- main.c
+++ main.c
@@ -9,8 +9,9 @@ int main(){
     int sameNumber = 0;//相同的数个数
     int sameLocation = 0;
     srand(time(0));//刷新时间
-    //并用户输入
+    //
     for (int i = 0;i <= 3;i++){
+        //产生随机数
         reality[i] = rand()%9+1;
         //判断是否有重复,成功
         for(int y = 0;y < i;){
@@ -20,17 +21,28 @@ int main(){
             }
             else y++;
         }
-        scanf("%d",&try[i]);
     }
     for (int i = 0;i <= 3;i++){
         //比较大小并排序，成功
         for (int x = 0;x < i;){
-            if (reality[x] >= reality[i]){
+            if (reality[x] >= reality[i]) {
                 tool = reality[x];
                 reality[x] = reality[i];
                 reality[i] = tool;
                 x = 0;
-            }else if (try[x] >= try[i]){
+            }
+            else x++;
+        }
+    }
+    printf("%d %d %d %d",reality[0],reality[1],reality[2],reality[3]);
+    do{
+        //用户输入
+        for (int i = 0;i <= 3;i++)
+            scanf("%d",&try[i]);
+        for (int i = 0;i <= 3;i++){
+        //比较大小并排序，成功
+        for (int x = 0;x < i;){
+            if (try[x] >= try[i]){
                 tool = try[x];
                 try[x] = try[i];
                 try[i] = tool;
@@ -42,16 +54,21 @@ int main(){
     //开始判断，判断有几个相同
     for (int i = 0;i <= 3;i++){
         for (int j =0;j <= 3;j++){
-            if (try[j] == reality[i])
+            if (try[j] == reality[i]){
                 sameNumber++;
+                break;
+            }
         }
-    }
-    for (int i = 0;i <= 3;i++){
-        if(reality[i] = try[i])
+        //判断几个数相同又对位
+        if(reality[i] == try[i])
             sameLocation++;
-        if(sameLocation == 4)
-            printf("成功！");
     }
-    printf("%dA%dB",sameLocation,sameNumber - sameLocation);
+    if(sameLocation == 4)
+        printf("成功！");
+    else {
+        printf("%dA%dB\n", sameLocation, sameNumber - sameLocation);
+        sameLocation = sameNumber = 0;
+    }
+    }while (sameLocation < 4);
     return 0;
 }
